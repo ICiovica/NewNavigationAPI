44b8b88dab3f3051bdf9c3c09e1b03b786e46692
Ionut Ciovica <ionut.ciovica@gmail.com>
2024-01-14 16:58:38 +0200

    Initial Commit

---
+++ NewNavigationAPI/NewNavigationAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewNavigationAPIApp.swift
+//  NewNavigationAPI
+//
+//  Created by IonutCiovica on 14/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct NewNavigationAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
